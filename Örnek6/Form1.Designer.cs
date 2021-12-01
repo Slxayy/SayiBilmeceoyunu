ff0972b382c06f9ffd4e4d7525a205198640465e
Balıq <yunusemrea939@gmail.com>
2021-12-01 20:45:41 +0300

    Proje tamamlandı

---
+++ Örnek6/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿
+namespace Örnek6
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.zorlukToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kolayToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ortaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.çıkışToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.White;
+            this.textBox1.Location = new System.Drawing.Point(135, 192);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(168, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.button1.Location = new System.Drawing.Point(182, 217);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Tahmin!";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.button2.Location = new System.Drawing.Point(167, 380);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 34);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Oyuna Başla";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(100, 266);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 22);
+            this.label1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(192, 243);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(10, 22);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "\r\n";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(206, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(10, 22);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "\r\n";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(163, 145);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(112, 44);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "\r\nHakkınız kaldı";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Palatino Linotype", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(132, -16);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(177, 64);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "\r\nSAYI BİLMECE";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.menuStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.menuStrip1.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.menuStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Visible;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(170, 421);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(95, 26);
+            this.menuStrip1.TabIndex = 8;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.zorlukToolStripMenuItem,
+            this.çıkışToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(83, 22);
+            this.toolStripMenuItem1.Text = "AYARLAR";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // zorlukToolStripMenuItem
+            // 
+            this.zorlukToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.kolayToolStripMenuItem,
+            this.ortaToolStripMenuItem,
+            this.zorToolStripMenuItem});
+            this.zorlukToolStripMenuItem.Name = "zorlukToolStripMenuItem";
+            this.zorlukToolStripMenuItem.Size = new System.Drawing.Size(117, 22);
+            this.zorlukToolStripMenuItem.Text = "Zorluk";
+            // 
+            // kolayToolStripMenuItem
+            // 
+            this.kolayToolStripMenuItem.Name = "kolayToolStripMenuItem";
+            this.kolayToolStripMenuItem.Size = new System.Drawing.Size(111, 22);
+            this.kolayToolStripMenuItem.Text = "Kolay";
+            this.kolayToolStripMenuItem.Click += new System.EventHandler(this.kolayToolStripMenuItem_Click);
+            // 
+            // ortaToolStripMenuItem
+            // 
+            this.ortaToolStripMenuItem.Name = "ortaToolStripMenuItem";
+            this.ortaToolStripMenuItem.Size = new System.Drawing.Size(111, 22);
+            this.ortaToolStripMenuItem.Text = "Orta";
+            this.ortaToolStripMenuItem.Click += new System.EventHandler(this.ortaToolStripMenuItem_Click);
+            // 
+            // zorToolStripMenuItem
+            // 
+            this.zorToolStripMenuItem.Name = "zorToolStripMenuItem";
+            this.zorToolStripMenuItem.Size = new System.Drawing.Size(111, 22);
+            this.zorToolStripMenuItem.Text = "Zor";
+            this.zorToolStripMenuItem.Click += new System.EventHandler(this.zorToolStripMenuItem_Click);
+            // 
+            // çıkışToolStripMenuItem
+            // 
+            this.çıkışToolStripMenuItem.Name = "çıkışToolStripMenuItem";
+            this.çıkışToolStripMenuItem.Size = new System.Drawing.Size(117, 22);
+            this.çıkışToolStripMenuItem.Text = "Çıkış";
+            this.çıkışToolStripMenuItem.Click += new System.EventHandler(this.çıkışToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Thistle;
+            this.ClientSize = new System.Drawing.Size(463, 450);
+            this.ControlBox = false;
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem zorlukToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kolayToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ortaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem çıkışToolStripMenuItem;
+    }
+}
+
---
+++ Örnek6/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Örnek6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int Sayi, hak;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            this.AcceptButton = button1;
+            MessageBox.Show("Oyun zorluğu ayarlar kısmında, bir zorluk seçmeniz gerekmektedir.");
+            MessageBox.Show("Toplam 10 hakkınız var");
+            MessageBox.Show("Oyuna başlamak için Oyuna başla butonuna basın.");
+        }
+
+        private void button(object sender, EventArgs e)
+        {
+            //yanlışlıkla basıldı
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            //yanlışlıkla basıldı
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                label2.Text = "Sayı giriniz.";
+            }
+            else
+            {
+                int girdiginSayi = int.Parse(textBox1.Text);
+
+                if (girdiginSayi < Sayi)
+                {
+                    label2.Text = "Yukarı";
+                    hak--;
+                }
+                else if (girdiginSayi > Sayi)
+                {
+                    label2.Text = "Aşağı";
+                    hak--;
+                }
+                else
+                {
+                    label2.Text = "İnanılmaz kazandın!!!";
+                    
+                }
+            }
+                    label3.Text = hak.ToString();
+
+            if (hak == 0)
+            {
+                
+                label1.Text = "Oyun bitti kaybettin :(... Sayı " + Sayi;
+            }
+        }
+
+        private void kolayToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Random rastgele = new Random();
+            Sayi = rastgele.Next(0, 10);
+        }
+
+        private void ortaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Random rastgele = new Random();
+            Sayi = rastgele.Next(0, 100);
+        }
+
+        private void zorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Random rastgele = new Random();
+            Sayi = rastgele.Next(0, 1000);
+        }
+
+        private void çıkışToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           
+
+            hak = 10;
+            label3.Text = hak.ToString();
+            label2.Text = "";
+            textBox1.Text = "";
+
+            button1.Enabled = true;
+           
+        }
+    }
+}
